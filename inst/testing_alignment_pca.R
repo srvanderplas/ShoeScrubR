8b99ffe9b11e4ef65b10cadbc6924b50475d58ad
Susan VanderPlas (root) <srvanderplas@gmail.com>
2019-09-21 19:10:48 -0500

    3 iterations...

--- inst/testing_alignment_pca.R
+++ inst/testing_alignment_pca.R
@@ -1,3 +1,7 @@
+library(tidyverse)
+library(EBImage)
+library(ShoeScrubR)
+
 test_img <- matrix(0, nrow = 49, ncol = 49) %>% as.Image()
 test_img[24:26,] <- 1
 test_img <- pad(test_img, 10, 10, 10, 10)
@@ -24,9 +28,19 @@ rotations <- tibble(img = list(test_img), angle = list(angle = seq(0, 180-5, 5))
          pca = purrr::map(rot_img, align_prcomp, weighted = T),
          est_angle = purrr::map(pca, get_angle)) %>%
   unnest(est_angle, .drop = F) %>%
-  mutate(fix_rot = purrr::map2(rot_img, -e1, rotate, output.dim = dim(test_img), bg.col = 0))
+  mutate(fix_rot = purrr::map2(rot_img, -e1, rotate, output.dim = dim(test_img), bg.col = 0)) %>%
+  nest(angle1 = c(e1:e6)) %>%
+  mutate(pca2 = purrr::map(fix_rot, align_prcomp, weighted = T),
+         est_angle = purrr::map(pca2, get_angle)) %>%
+  unnest(est_angle) %>%
+  mutate(fix_rot2 = purrr::map2(fix_rot, -(e1%%180), rotate, output.dim = dim(test_img), bg.col = 0)) %>%
+  nest(angle2 = c(e1:e6)) %>%
+  mutate(pca3 = purrr::map(fix_rot2, align_prcomp, weighted = T),
+         est_angle = purrr::map(pca3, get_angle)) %>%
+  unnest(est_angle) %>%
+  mutate(fix_rot3 = purrr::map2(fix_rot2, -(e1%%180), rotate, output.dim = dim(test_img), bg.col = 0))
 
 par(mfrow = c(6, 12))
 map(rotations$rot_img, plot)
-map(rotations$fix_rot, plot)
+map(rotations$fix_rot3, plot)
 
