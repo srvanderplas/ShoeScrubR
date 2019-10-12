9d531ce301f3737fe493bfc63ae039c87b61f9ba
Susan Vanderplas <srvanderplas@gmail.com>
2019-10-12 06:51:25 -0500

    Sample image

---
+++ inst/data-raw-001351R_20180124_5_1_1_csafe_hdzwart_Martin_Kruse-tif.R
@@ -0,0 +1,3 @@
+## code to prepare `data-raw/001351R_20180124_5_1_1_csafe_hdzwart_Martin_Kruse.tif` dataset goes here
+
+usethis::use_data("data-raw/001351R_20180124_5_1_1_csafe_hdzwart_Martin_Kruse.tif")
